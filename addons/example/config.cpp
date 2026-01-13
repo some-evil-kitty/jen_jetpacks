fd9c91cd805aeb5e9523c24a50751918432af270
some-evil-kitty <106408180+some-evil-kitty@users.noreply.github.com>
2026-01-13 14:46:29 -0500

    make hemtt happy

--- addons/example/config.cpp
+++ addons/example/config.cpp
@@ -4,8 +4,8 @@
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-        units[] = {};
-        weapons[] = {};
+        units[] = {"jen_jetpacks_example_mk5jetpack_base", "jen_jetpacks_example_mk5jetpack_nato", "jen_jetpacks_example_mk5jetpack_csat"};
+        weapons[] = {"jen_jetpacks_example_refuelitemmedium"};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"jen_jetpacks_core","A3_Weapons_F_Ammoboxes"}; // Include addons from this mod that contain code or assets you depend on. Affects loadorder. Including main as an example here.
         authors[] = {"Jenna", "Also Jenna","Also also Jenna","KJW too maybe"}; // sub array of authors, considered for the specific addon, can be removed or left empty {}
